af609a6a3cc0401c986c1e5b0b078ce5bd5626b4
Jiwoong CHOI <jwchoi179@soongsil.ac.kr>
2024-06-22 16:32:11 +0900

    [#13] View 파일 계층화

--- EatSSU-iOS/Migration/Source/Presentation/MyPage/View.swift
+++
@@ -1,8 +0,0 @@
-//
-//  View.swift
-//  EAT-SSU
-//
-//  Created by Jiwoong CHOI on 6/22/24.
-//
-
-import Foundation
---
+++ EatSSU-iOS/Migration/Source/Presentation/MyPageView/SubViews/DeletaAccountConfirmation/MigratedDeleteAccountConfirmationViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MigratedDeleteAccountConfirmationViewController.swift
+//  EAT-SSU
+//
+//  Created by Jiwoong CHOI on 6/22/24.
+//
+
+import UIKit
+
+class MigratedDeleteAccountConfirmationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
